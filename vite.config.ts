d5fc84a3cd598f6b8f9648fadbeb3c2263ac1aba
xmasuhai <xmasuhai@163.com>
2022-03-31 22:26:46 +0800

    重新部署â

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/mangosteen/', // build path
-  base: '/',
+  base: '/mangosteen-1/dist/', // build path
+  // base: '/',
   plugins: [vue()]
 })
