4020946334df99aaa0407ac0da6c7e4449ce4ac0
xmasuhai <xmasuhai@163.com>
2022-03-31 22:30:33 +0800

    ignore dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mangosteen-1/dist/', // build path
+  // base: '/mangosteen-1/dist/', // build path
   // base: '/',
   plugins: [vue()]
 })
