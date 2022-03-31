f0b0026cd6b06949655f76086583d4a60f067a20
xmasuhai <xmasuhai@163.com>
2022-03-31 23:05:34 +0800

    jsx代替template

---
+++ src/App2.tsx
@@ -0,0 +1,19 @@
+import {defineComponent,} from 'vue';
+
+export default defineComponent({
+  name: 'App2',
+  props: {},
+  components: {},
+  setup(/*props, ctx*/) {
+
+    return {};
+  },
+  render() {
+    return (
+      <>
+        App2.tsx
+      </>
+    );
+  }
+
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/mangosteen-1/dist/', // build path
   // base: '/',
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true
+    }),
+  ]
+});
